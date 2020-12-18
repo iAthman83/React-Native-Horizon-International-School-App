ba596f090d3b8af93f120a3d1db530210765be4f
lulu <athman.abu@gmail.com>
2020-12-18 12:40:01 +0300

    setup contact details for staff directory

---
+++ data/contactData.js
@@ -0,0 +1,76 @@
+import Contact from "../models/ContactsModel";
+
+export const CONTACTS = [
+  new Contact(
+    "st1",
+    "Mr.",
+    "Lokman",
+    "Cinar",
+    "Principal",
+    "+256 782 664 654",
+    "principal@horizon.ac.ug"
+  ),
+  new Contact(
+    "st3",
+    "Mr.",
+    "Fatih",
+    "Kabakci",
+    "Vice Principal",
+    "+256 781 772 700",
+    "vice_principal@horizon.ac.ug"
+  ),
+  new Contact(
+    "st4",
+    "Mr.",
+    "Yasin",
+    "Kabacir",
+    "Accountant",
+    "+256 781 772 700",
+    "vice_principal@horizon.ac.ug"
+  ),
+  new Contact(
+    "st5",
+    "Mr.",
+    "Chris",
+    "Kibirigye",
+    "Human Resource",
+    "+256 781 899 796",
+    "ckibirygye@horizon.ac.ug"
+  ),
+  new Contact(
+    "st6",
+    "Ms.",
+    "Rachael",
+    "Alyza",
+    "Administrator",
+    "+256 781 772 700",
+    "ralyza@horizon.ac.ug"
+  ),
+  new Contact(
+    "st7",
+    "Mr.",
+    "Bukenya",
+    "Ephraim",
+    "Seconday Cordinator",
+    "+256 781 772 700",
+    "bephraim@horizon.ac.ug"
+  ),
+  new Contact(
+    "st7",
+    "Mr.",
+    "Godfrey",
+    "Ssekitoleko",
+    "Primary Cordinator",
+    "+256 702 791 253",
+    "gssekitoleko@horizon.ac.ug"
+  ),
+  new Contact(
+    "st7",
+    "Mr.",
+    "Murat",
+    "Barkaci",
+    "Teacher",
+    "+256 781 772 700",
+    "vice_principal@horizon.ac.ug"
+  ),
+];
---
+++ models/ContactsModel.js
@@ -0,0 +1,19 @@
+class ContactModel {
+  constructor(
+    id,
+    salutation,
+    firstName,
+    lastName,
+    position,
+    phoneNumber,
+    email
+  ) {
+    this.id = id;
+    this.salutation = salutation;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.position = position;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+}
