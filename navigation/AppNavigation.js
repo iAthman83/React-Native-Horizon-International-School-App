2968a5f2e551b70fabbe2e2e5995f6e6edea1250
lulu <athman.abu@gmail.com>
2020-12-18 14:49:08 +0300

    will setup bottom navigation icons

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,15 +1,31 @@
+import React from "react";
 import { Platform } from "react-native";
 import Colors from "../constants/Colors";
 
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+// import icons
+import { Ionicons } from "@expo/vector-icons";
 
 // import all the screens
 import HomeScreen from "../screens/HomeScreen";
 import CalendarScreen from "../screens/CalendarScreen";
 import StaffDirectoryScreen from "../screens/StaffDirectoryScreen";
 import TimeTableScreen from "../screens/TimeTableScreen";
+import ContactDetailScreen from "../screens/ContactDetailScreen";
+import FavoriteContactScreen from "../screens/FavoriteContactScreen";
+
+// default stack navigation options
+const defaultstackNavOptions = {
+  // set up default styling and header names for the headers, as a second argument
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+};
 
 const AppNavigation = createStackNavigator(
   {
@@ -25,16 +41,73 @@ const AppNavigation = createStackNavigator(
     TimeTable: {
       screen: TimeTableScreen,
     },
+    ContactDetails: {
+      screen: ContactDetailScreen,
+    },
   },
   // set up default styling and header names for the headers, as a second argument
   {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
-      },
-      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
-    },
+    defaultNavigationOptions: defaultstackNavOptions,
   }
 );
 
-export default createAppContainer(AppNavigation);
+// Favorite contact navigator
+const FavNavigator = createStackNavigator(
+  {
+    Favorite: {
+      screen: FavoriteContactScreen,
+    },
+    ContactDetails: {
+      screen: ContactDetailScreen,
+    },
+  },
+  // set up default styling and header names for the headers, as a second argument
+  {
+    defaultNavigationOptions: defaultstackNavOptions,
+  }
+);
+
+// configutation for the bottom tabs
+const tabScreenConfig = {
+  Home: {
+    screen: AppNavigation,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name="ios-home" size={25} color={tabInfo.tintColor} />;
+      },
+      tabBarColor: Colors.accent,
+      tabBarLabel: Platform.OS === "android" ? <Text>Home</Text> : "Home",
+    },
+  },
+  Favorites: {
+    screen: FavNavigator,
+    navigationOptions: {
+      tabBarLabel: "Fav Contacts",
+      tabBarIcon: (tabInfo) => {
+        return (
+          <Ionicons name="ios-heart" size={25} color={tabInfo.tintColor} />
+        );
+      },
+      tabBarColor: Colors.primary,
+      tabBarLabel:
+        Platform.OS === "android" ? <Text>Fav Contacts</Text> : "Fav Contacts",
+    },
+  },
+};
+
+// bottom tab navigator tabs
+const ContactFavNavigator =
+  Platform.OS === "android"
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+        activeColor: "white",
+        shifting: true,
+      })
+    : createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: {
+          labelStyle: {},
+          activeTintColor: Colors.accentColor,
+        },
+      });
+
+export default createAppContainer(ContactFavNavigator);
---
+++ screens/ContactDetailScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ContactDetailScreen = (props) => {
+  return (
+    <View>
+      <Text>Time Table Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ContactDetailScreen;
